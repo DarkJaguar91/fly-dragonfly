c145780a166bcf1cf37bd72a68335a0626661d90
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-02-14 17:23:14 +0000

    fixed all problems, uploading the base libGDX files.

---
+++ trunk/fly-dragonfly-html/src/com/bmnb/fly_dragonfly/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.bmnb.fly_dragonfly.client;
+
+import com.bmnb.fly_dragonfly.Fly_Dragonfly;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Fly_Dragonfly();
+	}
+}
