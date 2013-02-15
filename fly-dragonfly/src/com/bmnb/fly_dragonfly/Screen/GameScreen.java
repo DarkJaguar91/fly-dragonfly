dde2fc11a362c3bd5f0ba797d8a69fa8f0cc3930
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-02-15 14:24:20 +0000

    Fixed the android upload issue. Also started a basic movement methodology.

---
+++ fly-dragonfly/src/com/bmnb/fly_dragonfly/Screen/GameScreen.java
@@ -0,0 +1,57 @@
+package com.bmnb.fly_dragonfly.Screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.bmnb.fly_dragonfly.InputProcessors.GameInputProcessor;
+import com.bmnb.fly_dragonfly.Objects.Player;
+
+public class GameScreen implements Screen {
+
+	public Player player;
+	protected SpriteBatch batch;
+	
+	@Override
+	public void render(float delta) {
+		player.update(delta);
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.begin();
+		player.Draw(batch);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+		batch = new SpriteBatch();
+		
+		player = new Player(new Vector2(), 50, 50, 0, 60, "data/square.png");
+		
+		Gdx.input.setInputProcessor(new GameInputProcessor(this));
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
