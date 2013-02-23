edc50bbc2c83211e0924288fb4f10ec96716d755
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-02-23 14:21:37 +0000

    Deleted old source, added new with the linking programs for now, will remove them later.

--- fly-dragonfly/src/com/bmnb/fly_dragonfly/Fly_DragonFly.java
+++ fly-dragonfly/src/com/bmnb/fly_dragonfly/Fly_DragonFly.java
@@ -1,7 +1,14 @@
 package com.bmnb.fly_dragonfly;
 
 import com.badlogic.gdx.Game;
-import com.bmnb.fly_dragonfly.Screen.GameScreen;
+import com.bmnb.fly_dragonfly.screens.GameScreen;
+
+/**
+ * Main class to start the game This calls upon the first screen (splash screen)
+ * 
+ * @author Brandon James Talbot
+ * 
+ */
 
 public class Fly_DragonFly extends Game {
 
--- fly-dragonfly/src/com/bmnb/fly_dragonfly/Objects/Player.java
+++ fly-dragonfly/src/com/bmnb/fly_dragonfly/Objects/Player.java
@@ -1,101 +1,85 @@
-/**
- * 
- */
-package com.bmnb.fly_dragonfly.Objects;
+package com.bmnb.fly_dragonfly.objects;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 /**
- * The player class, this houses all methods needed for the player object
+ * Player class, holds all methods needed for the player specifically
  * 
  * @author Brandon James Talbot
+ * 
  */
-public class Player extends DynamicObject {
+public class Player extends GameObject {
 
-	private Vector2 lastFingerPos;
+	protected static final float maxVertPath = 0.35f,
+								 upSpeedPercent = 0.8f,
+								 downSpeedPercent = 1.2f;
+
+	protected Vector2 targetPosition;
 
 	/**
 	 * Constructor
 	 * 
 	 * @param position
-	 *            The position of the object
 	 * @param width
-	 *            the width of the object
 	 * @param height
-	 *            the height of the object
-	 * @param angle
-	 *            the angle of the object
 	 * @param speed
-	 *            the speed of the object
-	 * @param texture
-	 *            the texture path
+	 * @param scWidth
+	 * @param scHeight
 	 */
-	public Player(Vector2 position, float width, float height, float angle,
-			float speed, String texture) {
-		super(position, width, height, angle, speed, texture);
-		lastFingerPos = position;
+	public Player(Vector2 position, float width, float height,
+			float speed, float scWidth, float scHeight) {
+		super(position, width, height, speed, scWidth, scHeight);
+		targetPosition = position;
 	}
 
 	@Override
-	protected void move(float delta) {
-		// calculate the new direction to move if the player wants it to move
-		Vector2 fingerpos = lastFingerPos.cpy();
-		Vector2 pos = position.cpy();
-
-		fingerpos.sub(pos);
-		fingerpos.nor();
-
-		direction = fingerpos.cpy();
-		// calculated the correct direction
-
-		/*
-		 * movement checking (if close to finger) the ship stops - note, since
-		 * we stop the ship from moving all the way up the screen we reach an
-		 * issue where the player could move off the screen. We thus have to add
-		 * a extra check for if the finger is too far up with the y value
-		 * section
-		 */
-		if (position.dst(lastFingerPos) <= speed * delta * direction.len()) {
-			direction = new Vector2();
-			position = lastFingerPos;
-		}
-		// end of close check
-
-		// x movement
-		this.position.x += this.direction.x * delta * speed;
-		// since the dragonfly is moving already (scrolling background)
-		// it makes sense that the fly cant move forward in the screen as
-		// quickly as backward
-		// this fixes this.
-		float ySpeed = direction.y > 0 ? speed / 2f : speed * 1.5f;
-		// y movement
-		this.position.y += this.direction.y > 0 ? this.position.y >= Gdx.graphics
-				.getHeight() * 0.2f ? 0 : this.direction.y * delta * ySpeed
-				: this.direction.y * delta * ySpeed;
-	}
-
-	@Override
-	public void stop() {
-		lastFingerPos = position.cpy();
-		super.stop();
+	protected void loadTexture() {
+		setTexture(new Texture("data/libgdx.png"));
 	}
 
 	/**
-	 * This is a method specific to the Player, it allows for moving towards the
-	 * finger position sent
-	 * 
-	 * @param fingerPos
-	 *            The position of the finger
+	 * Sets the new position the player should move to
+	 * @param fingerPos The position of the finger
 	 */
 	public void moveToFinger(Vector2 fingerPos) {
-		lastFingerPos = fingerPos.cpy();
+		targetPosition.x = fingerPos.x < this.getWidth() / 2f ? this.getWidth() / 2f
+				: fingerPos.x > screenWidth - this.getWidth() / 2f ? screenWidth
+						- this.getWidth() / 2f
+						: fingerPos.x;
 
-		lastFingerPos.y = lastFingerPos.y > Gdx.graphics.getHeight() * 0.2f
-				- height / 2f ? Gdx.graphics.getHeight() * 0.2f
-				: lastFingerPos.y < height / 2f ? height / 2f : lastFingerPos.y;
-		lastFingerPos.x = lastFingerPos.x > Gdx.graphics.getWidth() - width
-				/ 2f ? Gdx.graphics.getWidth() - width / 2f
-				: lastFingerPos.x < width / 2f ? width / 2f : lastFingerPos.x;
+		targetPosition.y = fingerPos.y < this.getHeight() / 2f ? this
+				.getHeight() / 2f
+				: fingerPos.y > screenHeight * maxVertPath ? screenHeight
+						* maxVertPath : fingerPos.y;
 	}
+
+	/**
+	 * Stops the player moving (should be called when finger is released)
+	 */
+	public void stopMovingToFinger() {
+		targetPosition = this.getPosition().cpy();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.bmnb.fly_dragonfly.objects.GameObject#move(float)
+	 */
+	@Override
+	protected void move(float delta) {
+		// check distancing
+		if (this.getPosition().cpy().sub(targetPosition).len() <= speed * delta){
+			targetPosition = this.getPosition().cpy();
+		}
+		
+		// calc new direction
+		Vector2 direction = targetPosition.cpy().sub(this.getPosition());
+		direction.nor();
+		
+		// move the player
+		this.translateX(speed * delta * direction.x);
+		this.translateY(speed * (direction.y < 0 ? upSpeedPercent : downSpeedPercent) * delta * direction.y);
+	}
+
 }
