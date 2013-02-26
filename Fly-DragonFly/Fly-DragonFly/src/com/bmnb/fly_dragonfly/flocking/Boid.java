16defe87790a18054cabf8e40b89d1b0393fcca3
bennahugo@gmail.com <bennahugo@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-02-26 21:00:44 +0000

    Boids

---
+++ Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/flocking/Boid.java
@@ -0,0 +1,37 @@
+package com.bmnb.fly_dragonfly.flocking;
+
+import com.badlogic.gdx.math.Vector2;
+import com.bmnb.fly_dragonfly.objects.GameObject;
+
+public class Boid extends GameObject{
+	private static final float MAX_SPEED = 35f;
+	private static final float MIN_SPEED = 25f;
+	
+	private Vector2 oldPosition;
+	public Boid(Vector2 position, Vector2 direction, float width,
+			float height, float scWidth, float scHeight){
+		super(position,width,height,(float)Math.random()*MAX_SPEED,scWidth,scHeight);
+	}
+	
+	public Vector2 getOldPosition() {
+		return oldPosition;
+	}
+	public void setOldPosition(Vector2 oldPosition) {
+		this.oldPosition = oldPosition;
+	}
+	public Vector2 getVelocity() {
+		return direction.cpy().mul(speed);
+	}
+	public void setVelocity(Vector2 velocity) {
+		this.speed = velocity.len();
+		this.direction = velocity.cpy().nor();
+	}
+
+	@Override
+	protected void move(float delta) {
+		setOldPosition(getPosition());
+		speed = Math.max(MIN_SPEED, Math.min(speed, MAX_SPEED)); //clamp speed 
+		getPosition().add(getVelocity().cpy().mul(speed).mul(delta));
+	}
+	
+}
