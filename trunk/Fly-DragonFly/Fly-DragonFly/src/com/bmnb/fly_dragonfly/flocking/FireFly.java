c82381d7ddbbf4e25a80211285fc9f0e60a6fdc4
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-03-09 10:39:07 +0000

    Collision detection and changed the firefly mossies percentage

--- trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/flocking/FireFly.java
+++ trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/flocking/FireFly.java
@@ -7,7 +7,7 @@ public class FireFly extends Boid {
 	public FireFly(Vector2 position, Vector2 direction, float width,
 			float height, float scWidth, float scHeight) {
 		super(position, direction, width, height, scWidth, scHeight);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
--- trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/map/Spawner.java
+++ trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/map/Spawner.java
@@ -3,6 +3,7 @@ package com.bmnb.fly_dragonfly.map;
 import com.badlogic.gdx.math.Vector2;
 import com.bmnb.fly_dragonfly.flocking.BoidsModel;
 import com.bmnb.fly_dragonfly.objects.Bird;
+import com.bmnb.fly_dragonfly.objects.Frog;
 import com.bmnb.fly_dragonfly.objects.Player;
 import com.bmnb.fly_dragonfly.objects.Spider;
 import com.bmnb.fly_dragonfly.objects.VenusFlytrap;
@@ -35,6 +36,9 @@ public class Spawner {
 							_scrollSpeed, GameScreen.width, GameScreen.height,  _player));
 				else if (os.getType() == ObjectSpawner.Type.OS_VENUSFT) 
 					GameScreen.addObject(new VenusFlytrap(new Vector2(os.getX(),os.getY()), os.getWidth(), os.getHeight(), 
+						_scrollSpeed, GameScreen.width, GameScreen.height,  _player));
+				else if (os.getType() == ObjectSpawner.Type.OS_FROG) 
+					GameScreen.addObject(new Frog(new Vector2(os.getX(),os.getY()), os.getWidth(), os.getHeight(), 
 						_scrollSpeed, GameScreen.width, GameScreen.height,  _player));
 				_map.getGameObjects().remove(i--);
 			}	
