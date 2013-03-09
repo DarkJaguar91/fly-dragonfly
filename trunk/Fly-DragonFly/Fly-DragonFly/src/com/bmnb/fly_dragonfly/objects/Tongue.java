1e4b64a77c40ae2dd7ea4393e93855ecfe3f14c3
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-03-09 13:30:34 +0000

    fixed the frogs aim

--- trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/objects/Tongue.java
+++ trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/objects/Tongue.java
@@ -4,49 +4,56 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
-public class Tongue extends StaticEnemy{
+public class Tongue extends StaticEnemy {
 
-	
-	public static final float growRate = 0.1f;//percentage
+	public static final float growRate = 0.7f;// percentage
 	protected float length;
 	protected Vector2 target;
 	protected boolean grow = true;
-	
-	
+
 	public Tongue(Vector2 position, float width, float height, float speed,
 			float scWidth, float scHeight, Player player) {
 		super(position, width, height, speed, scWidth, scHeight, player);
-		
+
 		sortVal = 3;
+
+		target = player.getPosition().cpy();
 		
-		target = player.getPosition().cpy().add(player.direction.cpy().mul(player.speed * (1 / growRate))); // calculate pos further away
-		
-		length = target.cpy().sub(this.getPosition()).len();
+		target.add((new Vector2(0, 1)).mul(this.speed * 1.5f));
+
+		length = target.cpy().sub(this.getPosition().cpy()).len() * 1.5f;
+
+		//this.setSize(100, 0);
 
 		float angle = (target.cpy().sub(this.getPosition().cpy())).angle();
 		
-		//this.setRotation(angle);
-		
-		this.setTexture(new Texture("data/square.png"));
+		this.setRotation(angle - 90);
+
+		this.setTexture(new Texture("data/tongue.png"));
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.bmnb.fly_dragonfly.objects.GameObject#update(float)
 	 */
 	@Override
 	public void update(float delta) {
-//		if (this.getHeight() >= length){
-//			grow = false;
-//		}
-//		
-//		this.setSize(this.getWidth(), this.getHeight() + (grow ? length * growRate : - growRate * length));
-		//Gdx.app.log("FrogTongue", "Growing - " + this.getHeight());
-		
-		
+		if (this.getHeight() >= length) {
+			grow = false;
+		}
+
+		this.setSize(this.getWidth(), this.getHeight()
+				+ (grow ? length * delta * growRate : -growRate * length
+						* delta));
+		// Gdx.app.log("FrogTongue", "Growing - " + this.getHeight());
+
 		super.update(delta);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.bmnb.fly_dragonfly.objects.StaticEnemy#move(float)
 	 */
 	@Override
@@ -54,5 +61,21 @@ public class Tongue extends StaticEnemy{
 		this.target = target.cpy().sub(new Vector2(0, delta * speed));
 		super.move(delta);
 	}
-	
+
+	@Override
+	public void draw(SpriteBatch spriteBatch) {
+		spriteBatch.draw(this.getTexture(), this.getX() - this.getWidth() / 2,
+				this.getY(), this.getWidth()/2, -this.getHeight(), this.getWidth(), this
+						.getHeight(), 1, 1, this.getRotation(), 0, 0, this
+						.getTexture().getWidth(),
+				this.getTexture().getHeight(), false, false);
+		// super.draw(spriteBatch);
+	}
+
+	@Override
+	public void kill() {
+		this.removeable = true;
+		super.kill();
+	}
+
 }
