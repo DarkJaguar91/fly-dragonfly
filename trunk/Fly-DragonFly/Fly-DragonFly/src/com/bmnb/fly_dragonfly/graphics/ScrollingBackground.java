076603680a49093d75052660dc54a3b393f302a4
bjtal91@gmail.com <bjtal91@gmail.com@797f6cf2-fbc3-4268-a139-046bebff5f6a>
2013-03-10 07:54:22 +0000

    Updated gameobject (since you guys are now updating the objects twice, just removed it from the draw method). Changed the scroller a little

--- trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/graphics/ScrollingBackground.java
+++ trunk/Fly-DragonFly/Fly-DragonFly/src/com/bmnb/fly_dragonfly/graphics/ScrollingBackground.java
@@ -4,62 +4,75 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-
 /**
- * This class holds the methods required to draw the background which scrolls dynamically
+ * This class holds the methods required to draw the background which scrolls
+ * dynamically
  * 
  * @author Brandon James Talbot
- *
+ * 
  */
 
 public class ScrollingBackground {
-	
+
 	/**
 	 * Globals
 	 */
 	protected Sprite[] sprites;
 	protected float width, height, speed;
-	
+
 	/**
 	 * Constructor
-	 * @param texture path to the texture
-	 * @param scwidth The screen width
-	 * @param scheight The screen height
-	 * @param speed The speed for the scrolling
+	 * 
+	 * @param textures
+	 *            path to the textures
+	 * @param scwidth
+	 *            The screen width
+	 * @param scheight
+	 *            The screen height
+	 * @param speed
+	 *            The speed for the scrolling
 	 */
-	public ScrollingBackground (String[] textures, float scwidth, float scheight, float speed){
+	public ScrollingBackground(String[] textures, float scwidth,
+			float scheight, float speed) {
 		sprites = new Sprite[textures.length];
 		width = scwidth;
-		
-		for (int i = 0; i < textures.length; ++i){
+
+		for (int i = 0; i < textures.length; ++i) {
 			Texture t = new Texture(textures[i]);
 			height = width / t.getWidth() * t.getHeight();
 			sprites[i] = new Sprite(t);
 			sprites[i].setSize(width, height);
 			sprites[i].setPosition(0, i * height);
-			sprites[i].setColor(0.6f,0.6f,0.6f,1);
+			sprites[i].setColor(0.5f, 0.5f, 0.5f, 1);
 		}
-		
+
 		this.speed = speed;
 	}
-	
-	public void update(float delta){
-		for(int i = 0;i<sprites.length;++i){
+
+	/**
+	 * Updates the backgrounds positions
+	 * @param delta The games delta change
+	 */
+	public void update(float delta) {
+		for (int i = 0; i < sprites.length; ++i) {
 			sprites[i].translateY(-speed * delta);
-			
+
 			// move the tex above when it leaves the screen
-			if (sprites[i].getY() + height <= 0){
+			if (sprites[i].getY() + height <= 0) {
 				sprites[i].translateY(height * sprites.length);
 			}
 		}
 	}
-	
+
 	/**
 	 * Draws the background
-	 * @param batch Sprite batch to draw with
-	 * @param delta The delta time for the game
+	 * 
+	 * @param batch
+	 *            Sprite batch to draw with
+	 * @param delta
+	 *            The delta time for the game
 	 */
-	public void draw(SpriteBatch batch, float delta){
+	public void draw(SpriteBatch batch, float delta) {
 		for (Sprite s : sprites)
 			s.draw(batch);
 	}
